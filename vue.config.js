5c03f2b032b7c5eea8799609efb12ed33bcdae41
Salih <salih@Salih-MacBook-Pro.local>
2022-02-11 00:09:44 +0300

    genel yapilandirma (1)

---
+++ vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: 'tr',
+      fallbackLocale: 'tr',
+      localeDir: 'locales',
+      enableLegacy: false,
+      runtimeOnly: false,
+      compositionOnly: false,
+      fullInstall: true
+    }
+  }
+}
