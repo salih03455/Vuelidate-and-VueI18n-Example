6f4d6a87a6b4272072ddf8fbb84263309f7e598c
Salih <salih@Salih-MacBook-Pro.local>
2022-02-11 16:19:07 +0300

    storeda locale metodlarini olusturdum

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,20 @@
 import { createStore } from 'vuex'
+import { selectedLocale } from '@/i18n'
 
 export default createStore({
   state: {
+    locale: selectedLocale
   },
   mutations: {
+    updateLocale(state, newLocale) {
+      state.locale = newLocale
+    }
   },
   actions: {
+    changeLocale({ commit }, newLocale) {
+      localStorage.setItem('lang', newLocale)
+      commit('updateLocale', newLocale)
+    }
   },
   modules: {
   }
